88d6f46aa362cf8b5fa3f9810cac86a7541728f1
Vinay_Sharma <vinay@elred.io>
2024-07-10 18:17:27 +0530

    plugin code in isolate

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:platform_channels/src/random_no_method_channel.dart';
 import 'package:platform_channels/src/timer_event_channel.dart';
 
+void isolateMain(RootIsolateToken rootIsolateToken) async {
+  BackgroundIsolateBinaryMessenger.ensureInitialized(rootIsolateToken);
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  print('App version info ${packageInfo.version}');
+}
+
 void main() {
+  RootIsolateToken rootIsolateToken = RootIsolateToken.instance!;
+  Isolate.spawn(isolateMain, rootIsolateToken);
   runApp(const MainApp());
 }
 
